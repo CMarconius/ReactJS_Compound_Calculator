4781344f171c9bb467f2b1aa6a25c47632b464fc
Marconius <marconius.dev@gmail.com>
2021-06-19 18:36:55 +0100

    First Upload of Content

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Components/Calculator'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+
+        <Navbar />
+
+
+        <Calculator />
+
+
+        <Footer />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
